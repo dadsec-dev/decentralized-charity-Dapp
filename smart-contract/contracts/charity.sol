78eaebd19514c3c67197dd8efc1fed8b8577f603
dadsec-dev <geecypher41@gmail.com>
2023-06-29 15:59:03 +0100

    working on contract still

--- smart-contract/contracts/charity.sol
+++ smart-contract/contracts/charity.sol
@@ -17,16 +17,18 @@ contract charity {
         string description;
         uint256 goal;
         uint256 currentAmount;
-        mapping (address => uint256) individualContribution;
         uint256 starttime;
         uint256 endtime;
         bool isStarted;
         bool isClosed;
     }
+    
+    
 
     mapping (uint256 => project) projects;
 
     uint256 public totalProjects;
+    uint256[] public allProjectsArray;
     uint256 public PlatformFee = 0.01 ether;
 
     event projectCreated (uint256 projectId, address creator, uint256 goalAmount);
@@ -56,6 +58,9 @@ contract charity {
         project_.starttime = _starttime;
         project_.endtime = _endtime;
         project_.isStarted = true;
+
+        totalProjects = totalProjects + 1;
+        allProjectsArray.push(_currentProjectId);
         
         emit projectCreated(_currentProjectId, msg.sender, _goal);
     }
@@ -64,14 +69,13 @@ contract charity {
         
         project storage project_ = projects[projectId_];
 
-        require(project_.isStarted == true, "not started");
+        require(project_.isStarted == true, "not started or ended");
         require(block.timestamp < project_.endtime, "ended" );
 
         if (project_.isStarted == true) {
 
             // project_.contributions = project_.contributions++;
             project_.contributors.push(msg.sender);
-            project_.individualContribution[msg.sender] = msg.value;
             project_.currentAmount += msg.value;
 
 
@@ -80,10 +84,84 @@ contract charity {
            revert projectEnded__();
         }
 
+        if (block.timestamp >= project_.endtime) {
+            project_.isStarted == false;
+            project_.isClosed == true;
+        }
+
+        emit projectFunded(projectId_, msg.sender, msg.value);
+
+
+
 
 
         
 
+    }
+
+    function getTotalNumberOfProjects() public view returns (uint256) {
+        return totalProjects;
+    }
+
+
+
+
+
+    function getallProjects() public view returns (project[] memory) {
+
+       uint256 totalprojects_ = currentProjectId.current();
+       project[] memory allProjects = new project[](totalprojects_);
+
+       for (uint256 i = 0; i < allProjects.length; i ++) {
+            allProjects[i - 1] = allProjects[i];
+       }
+       return allProjects;
+    }
+
+    function getOnGoingProjects() public view returns (project[] memory ) {
+        uint256 count = 0;
+
+        for (uint256 i = 1; i < currentProjectId.current(); i++) {
+            if(projects[i].isStarted && !projects[i].isClosed) {
+                count++;
+            }
+        }
+
+
+        project[] memory onGoingProjects = new project[](count);
+        uint256 index = 0;
+
+        for (uint256 i = 0; i <= currentProjectId.current(); i++) {
+            if(projects[i].isStarted && !projects[i].isClosed) {
+                onGoingProjects[index] = projects[i];
+                index++;
+            }
+        }
+
+        return onGoingProjects;
+    }
+
+    function getClosedProjects() public view returns (project[] memory) {
+        uint count = 0;
+
+        for (uint256 i = 0; i < currentProjectId.current(); i++) {
+            if (projects[i].isClosed) {
+                count++;
+            }
+        }
+
+        project[] memory closedProjects = new project[](count);
+
+        uint256 index = 0;
+
+        for(uint256 i = 0; i <= currentProjectId.current(); i++) {
+            if(projects[i].isClosed) {
+                closedProjects[index] = projects[i];
+                index++;
+            }
+        }
+
+        return closedProjects;
     }
 
 
