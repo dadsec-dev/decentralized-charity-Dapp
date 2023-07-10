d36ed311818b96752eacadc55b03dadb07735319
dadsec-dev <geecypher41@gmail.com>
2023-07-10 20:48:59 +0100

    updated the DAO

--- smart-contract/contracts/DAO.sol
+++ smart-contract/contracts/DAO.sol
@@ -18,9 +18,12 @@ contract charityDAO {
     struct proposal {
         address proposer;
         string description;
-        uint256 votingPeriod;
+        uint256 votingStart;
+        uint256 votingEnd;
         address[] pvoters;
         uint256 totalVotes;
+        uint256 yes;
+        uint256 no;
 
     }
 
@@ -28,16 +31,20 @@ contract charityDAO {
     mapping (uint256 => mapping (address => bool) ) hasVoted;
 
     event proposalCreated(uint256 proposalId, string description, address proposer);
-    event voteCasted(uint256 proposalId, address voter);
+    event voteCasted(uint256 proposalId, address voter, bool _voteOption);
+
+    error votingEnded();
 
     function setDAOtoken(address account) public {
         require(msg.sender == platform, "Unauthorized entity");
         charityToken = charityRewardToken(account);
     }
 
-    function createProposal(string memory _text, uint256 _votingPeriod) public {
+    function createProposal(string memory _text, uint256 _startTime, uint256 _endTime) public {
         require(charityToken.balanceOf(msg.sender) >= 1000, "Must hold DAO token");
         require(msg.sender != address(0), "invalid address");
+        require(block.timestamp <= _startTime, "Must me a time in the future or currentTime");
+        require(block.timestamp <= _endTime && _startTime < _endTime, "Invalid time setting");
 
         proposalID.increment();
 
@@ -46,20 +53,59 @@ contract charityDAO {
         proposal memory proposalCreation = proposalDetails[currentProposal];
 
         proposalCreation.proposer = msg.sender;
-        proposalCreation.votingPeriod = _votingPeriod;
+        proposalCreation.votingStart = _startTime;
+        proposalCreation.votingEnd = _endTime;
         proposalCreation.description = _text;
 
 
     }
 
 
-    function voteProposal(uint256 proposalId) public {
+    function voteProposal(uint256 _proposalId, bool _voteOption) public {
         require(charityToken.balanceOf(msg.sender) >= 1000, "Must hold DAO token");
-        require(msg.sender != proposalDetails[proposalId].proposer, "Proposer cannot vote");
+        require(block.timestamp <= proposalDetails[_proposalId].votingStart, "has not started");
+        if (block.timestamp > proposalDetails[_proposalId].votingEnd) {
+            revert votingEnded();
+        }
+        require(msg.sender != proposalDetails[_proposalId].proposer, "Proposer cannot vote");
+        require(!hasVoted[_proposalId][msg.sender], "Address has voted");
 
-        proposal storage _prop = proposalDetails[proposalId];
+        proposal storage _prop = proposalDetails[_proposalId];
         _prop.pvoters.push(msg.sender);
         _prop.totalVotes = _prop.totalVotes + 1;
+        hasVoted[_proposalId][msg.sender] = true;
+
+        if (_voteOption) {
+            if (charityToken.balanceOf(msg.sender) >= 10000) {
+                _prop.yes = _prop.yes + 10;
+            } else if (charityToken.balanceOf(msg.sender) < 10000 && charityToken.balanceOf(msg.sender) >= 4998 ){
+                _prop.yes = _prop.yes + 5;
+            } else {
+                _prop.yes = _prop.yes + 1;
+            }
+            
+        } else {
+            if (charityToken.balanceOf(msg.sender) >= 10000) {
+                 _prop.no = _prop.no + 10;
+            } else if (charityToken.balanceOf(msg.sender) < 10000 && charityToken.balanceOf(msg.sender) >= 4998 ){
+                 _prop.no = _prop.no + 5;
+            } else {
+                _prop.yes = _prop.yes + 1;
+            }
+           
+        }
+
+        emit voteCasted(_proposalId, msg.sender, _voteOption);
 
     }
+
+    function returnYes(uint256 _proposalId) public view returns (uint256) {
+        return proposalDetails[_proposalId].yes;
+    }
+
+    function returnNo(uint256 _proposalId) public view returns (uint256) {
+        return proposalDetails[_proposalId].no;
+    }
+
+    
 }
